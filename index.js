0b932b7130d023a9cb7f63727de6961c1b7c7893
Aidan Margo <aidanmargo@gmail.com>
2021-09-20 10:24:18 -0500

    commit tree 1e35e3d7c927d2cc57ee919b329a8769ebc8f28d

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+
+const currentUser = 'Aidan Margo';
+// const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+// const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.toUpperCase() + '!';
+// const shortGreeting = 'Welcome, ' + currentUser[0] + '!';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser[0]}!`
+
+console.log(excitedWelcomeMessage);
